09ba069936f659905dda61e931146e4ab481beca
高伟军 <gaoweijung@gmail.com>
2019-11-18 18:13:37 +0800

    安装python shadowsocks脚本 开启shadowsocks脚本

---
+++ install.shadowsocks.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+yum install -y python-setuptools && easy_install pip && pip install shadowsocks
---
+++ start.shadowsocks.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+ssserver -p 4396 -k houtougao -m aes-256-cfb --user nobody -d start
